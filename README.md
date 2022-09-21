b80ea4f4f80f1c1bb9daad5fac663f3f482a956b
leOhsantos <leonardo.santos191004@gmail.com>
2022-09-21 16:56:23 -0300

    adicionando validação de form e atualizando layout

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
+<div align="center">
+<img src="assets/img/logo-completa.png" width="330em">
+</div>
+
+<br>
+
 # Site do BuscaSUS
 
-Site criado para conhecer o BuscaSUS, um projeto criado para o trabalho de conclusão de curso da ETEC de Guaianzes
+Site criado para conhecer o BuscaSUS, um projeto criado para o trabalho de conclusão de curso da ETEC de Guaianazes.
 
-# 🚀 Skills
+<br>
 
+## 🚀 Skills
 
-# 👩‍🚀 Desenvolvedores
+<div align="left">
+ <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+ <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
+ <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+</div>
+
+<br>
+
+## 💻 Desenvolvedores
+
+‣ 👨‍🚀 [Leonardo Santos](https://github.com/leOhsantos)
+<br>
+‣ 👨‍🚀 [Yuri Oliveira](https://github.com/YuriOlivs)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,74 @@
-const navbar = document.getElementById('navbar');
+const navbar = document.getElementById("navbar"),
+    form = document.getElementById("form"),
+    emailInput = document.getElementById("emailUser"),
+    assuntoInput = document.getElementById("assuntoUser"),
+    msgInput = document.getElementById("msgUser"),
+    errorText = document.getElementsByClassName("error-text"),
+    submitBtn = document.getElementById("submitBtn");
 
-const ativarScroll = () => {
-    const windowTop = window.pageYOffset + (window.innerHeight * 1);
-
-    navbar.classList.toggle('navbar-active', scrollY > 0)
+function changeNav() {
+    navbar.classList.toggle("navbar-active", scrollY > 0);
 }
 
-window.addEventListener("scroll", ativarScroll)
+function validateInputs() {
+    const regexEmail = /\S+@\S+\.\S+/,
+        emailTest = regexEmail.test(emailInput.value),
+        assuntoValue = assuntoInput.value,
+        msgValue = msgInput.value;
+
+    if (emailTest === false) {
+        emailInput.classList.add("input-error");
+        errorText[0].classList.add("active");
+    } else {
+        emailInput.classList.remove("input-error");
+        errorText[0].classList.remove("active");
+    }
+
+    if (assuntoValue === "") {
+        assuntoInput.classList.add("input-error");
+        errorText[1].classList.add("active");
+    } else {
+        assuntoInput.classList.remove("input-error");
+        errorText[1].classList.remove("active");
+    }
+
+    if (msgValue === "") {
+        msgInput.classList.add("input-error");
+        errorText[2].classList.add("active");
+    } else {
+        msgInput.classList.remove("input-error");
+        errorText[2].classList.remove("active");
+    }
+
+    if (emailTest === true && assuntoValue !== "" && msgValue !== "") {
+        form.submit();
+    }
+}
+
+function removeInputsError() {
+    const regexEmail = /\S+@\S+\.\S+/,
+        emailTest = regexEmail.test(emailInput.value),
+        assuntoValue = assuntoInput.value,
+        msgValue = msgInput.value;
+
+    if (emailTest === true) {
+        emailInput.classList.remove("input-error");
+        errorText[0].classList.remove("active");
+    }
+
+    if (assuntoValue !== "") {
+        assuntoInput.classList.remove("input-error");
+        errorText[1].classList.remove("active");
+    }
+
+    if (msgValue !== "") {
+        msgInput.classList.remove("input-error");
+        errorText[2].classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", changeNav);
+submitBtn.addEventListener("click", validateInputs);
+emailInput.addEventListener("input", removeInputsError);
+assuntoInput.addEventListener("input", removeInputsError);
+msgInput.addEventListener("input", removeInputsError);
